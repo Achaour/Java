7142ffd013f91c517379133f3660512fed8e42fd
Mansour Achaour <achaourmansour@gmail.com>
2022-06-08 17:49:04 +0200

    Exercices

---
+++ src/Exercice3.java
@@ -0,0 +1,21 @@
+public class Exercice3 {
+    public static void main(String[] args) {
+        String prenom="Mansour";
+        String nom="Achaour";
+       // String vide= " ";
+        byte age=24;
+        System.out.print("Vous vous appelez " +prenom + " " + nom+ " et vous avez " +age+ " ans");
+        //System.out.print("Vous vous appelez");
+        //System.out.print(vide);
+        // System.out.print(prenom);
+        //System.out.print(vide);
+        //System.out.print(nom);
+        //System.out.print(", et vous avez");
+        //System.out.print(vide);
+        //System.out.print(age);
+        //System.out.print(vide);
+        //System.out.print("ans.");
+    }
+}
+
+
---
+++ src/Exercice5.java
@@ -0,0 +1,12 @@
+public class Exercice5 {
+
+    public static void main(String[] args) {
+        System.out.println("votre age svp !");
+        byte age=24;
+        System.out.println("mon age est:" + age);
+        int annee= 2022-age;
+        System.out.println("Votre annee de naissance est " + annee);
+
+
+    }
+}
